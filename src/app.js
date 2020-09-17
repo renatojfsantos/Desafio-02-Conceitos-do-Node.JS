f51ec8dad9dd5cfcefb911dbe6b8a03820a4e4d8
Renato Santos <renato_jfs@hotmail.com>
2020-09-16 22:37:10 -0300

    Route delete

--- src/app.js
+++ src/app.js
@@ -35,7 +35,19 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repository =>
+    repository.id === id
+  );
+
+  if (findRepositoryIndex >= 0) {
+    repositories.splice(findRepositoryIndex, 1);
+  } else {
+    return response.status(400).json({ error: 'Repository does not exists.' });
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
